f42d25540533ef8ecf25beaecdca5cd8f635ced8
Chris Grych <ceggrych@gmail.com>
2019-11-20 18:10:14 -0600

    Adds init dash. Starts crash ability.

--- Assets/Scripts/Player/PlayerAbilities.cs
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -9,7 +9,12 @@ public class PlayerAbilities : MonoBehaviour
     [SerializeField] private int numberOfDashes = 3;
     [Tooltip("How long it takes before dash can be used again")]
     [SerializeField] private float dashRechargeTime = 2f;
-    [SerializeField] private float dashProtectionTimeInSeconds = 2f;
+    [SerializeField] private ParticleSystem dashParticles;
+
+    [Header("Crash Ability")]
+    [SerializeField] private int numberOfCrashes = 1;
+    [SerializeField] private float crashRechargeTime = 4f;
+    [SerializeField] private ParticleSystem crashParticles;
 
     private bool canCharge = true;
 
@@ -29,12 +34,17 @@ public class PlayerAbilities : MonoBehaviour
         {
             Dash();
         }
+        if (Input.GetButtonDown("Crash"))
+        {
+            Crash();
+        }
     }
 
     private void Dash()
     {
         if (numberOfDashes > 0)
         {
+            dashParticles.Play();
             anim.SetTrigger("Dash");
             playerRb.AddForce(playerRb.velocity * dashForce, ForceMode.Impulse);
             numberOfDashes--;
@@ -46,5 +56,22 @@ public class PlayerAbilities : MonoBehaviour
     {
         yield return new WaitForSeconds(dashRechargeTime);
         numberOfDashes++;
+    }
+
+    private void Crash()
+    {
+        if (numberOfCrashes == 0)
+        {
+            crashParticles.Play();
+            anim.SetTrigger("Crash");
+            numberOfCrashes--;
+            StartCoroutine(CrashRecharge());
+        }
+    }
+
+    private IEnumerator CrashRecharge()
+    {
+        yield return new WaitForSeconds(crashRechargeTime);
+        numberOfCrashes++;
     }
 }
