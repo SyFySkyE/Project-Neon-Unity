20f4faefdb572e3f641ae52e527e9036b1223d3f
Chris Grych <ceggrych@gmail.com>
2019-12-22 21:39:22 -0600

    Adds set up for move and aim tutorial

---
+++ Assets/MoveAimTutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAimTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject moveText;
+    [SerializeField] private GameObject aimText;
+    [SerializeField] private float textFadeInSeconds = 3f;
+     
+
+
+    private IEnumerator FadeText(GameObject textObj)
+    {
+        yield return new WaitForSeconds(textFadeInSeconds);
+        textObj.SetActive(false);
+    }
+
+    private void OnDestroy() // If the player dies
+    {
+        moveText.SetActive(false);
+        aimText.SetActive(false);
+    }
+}
--- Assets/ShootTutorial.cs
+++ Assets/ShootTutorial.cs
@@ -7,7 +7,12 @@ public class ShootTutorial : MonoBehaviour
 {
     [SerializeField] private GameObject enemyToSpawn;
     [SerializeField] private Transform enemySpawnLoc;
+    [SerializeField] private GameObject shootText;    
 
+    private enum TutorialState { Shooting, Dashing, Crashing, Overdrive }
+    private TutorialState currentState = TutorialState.Shooting;
+
+    private bool isTimeStopped = false;
     private BoxCollider cutsceneTrigger;
     private PlayableDirector shootCutsceneTimeline;
 
@@ -25,5 +30,48 @@ public class ShootTutorial : MonoBehaviour
             shootCutsceneTimeline.Play();
             cutsceneTrigger.enabled = false;
         }
+    }
+
+    public void SpawnRunEnemy()
+    {
+        Instantiate(enemyToSpawn, enemySpawnLoc.position, Quaternion.identity);
+        StopTimeAndDisplayShootText();
+    }
+
+    private void StopTimeAndDisplayShootText()
+    {
+        ToggleTime();
+        shootText.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (isTimeStopped)
+        {
+            switch (currentState)
+            {
+                case TutorialState.Shooting:
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        ToggleTime();
+                        shootText.SetActive(false);
+                        this.currentState = TutorialState.Dashing;
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void ToggleTime()
+    {        
+        if (isTimeStopped)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+        isTimeStopped = !isTimeStopped;
     }
 }
